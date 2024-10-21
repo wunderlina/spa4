a2e43a0cd991004db030b0d5fcf0bd2c33abbd0f
Alex Wunderlin <wunderlina@msoe.edu>
2024-10-21 15:31:03 -0500

    Created Repository

---
+++ cmake-build-debug/CMakeFiles/temp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temp.dir/main.cpp.obj"
+  "CMakeFiles/temp.dir/main.cpp.obj.d"
+  "libtemp.dll.a"
+  "temp.exe"
+  "temp.exe.manifest"
+  "temp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
